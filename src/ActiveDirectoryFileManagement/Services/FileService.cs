bcfb6699bd1e63633e377551091ce3853b24fc1e
ShadyNagy <shadynagi@gmail.com>
2024-03-03 15:14:40 +0200

    FileService xml comments are created.

--- src/ActiveDirectoryFileManagement/Services/FileService.cs
+++ src/ActiveDirectoryFileManagement/Services/FileService.cs
@@ -11,6 +11,11 @@ public class FileService : IFileService
 		_activeDirectoryService = activeDirectoryService;
 	}
 
+	/// <summary>
+	/// Creates a new file with specified content under the context of an Active Directory user.
+	/// </summary>
+	/// <param name="path">The path where the file will be created.</param>
+	/// <param name="content">The string content to write to the file.</param>
 	public void CreateUnderUser(string path, string content)
 	{
 		_activeDirectoryService.ImpersonateUserAndRunAction(() =>
@@ -22,6 +27,11 @@ public class FileService : IFileService
 		});
 	}
 
+	/// <summary>
+	/// Creates a new file with specified content.
+	/// </summary>
+	/// <param name="path">The path where the file will be created.</param>
+	/// <param name="content">The string content to write to the file.</param>
 	public void Create(string path, string content)
 	{
 		if (!File.Exists(path))
@@ -30,6 +40,11 @@ public class FileService : IFileService
 		}
 	}
 
+	/// <summary>
+	/// Creates a new file with specified byte content under the context of an Active Directory user.
+	/// </summary>
+	/// <param name="path">The path where the file will be created.</param>
+	/// <param name="contentBytes">The byte array content to write to the file.</param>
 	public void CreateUnderUser(string path, byte[] contentBytes)
 	{
 		_activeDirectoryService.ImpersonateUserAndRunAction(() =>
@@ -41,6 +56,11 @@ public class FileService : IFileService
 		});
 	}
 
+	/// <summary>
+	/// Creates a new file with specified byte content.
+	/// </summary>
+	/// <param name="path">The path where the file will be created.</param>
+	/// <param name="contentBytes">The byte array content to write to the file.</param>
 	public void Create(string path, byte[] contentBytes)
 	{
 		if (!File.Exists(path))
@@ -49,6 +69,11 @@ public class FileService : IFileService
 		}
 	}
 
+	/// <summary>
+	/// Creates a new file and writes a sequence of lines under the context of an Active Directory user.
+	/// </summary>
+	/// <param name="path">The path where the file will be created.</param>
+	/// <param name="lines">The lines to write to the file.</param>
 	public void CreateUnderUser(string path, IEnumerable<string> lines)
 	{
 		_activeDirectoryService.ImpersonateUserAndRunAction(() =>
@@ -60,6 +85,11 @@ public class FileService : IFileService
 		});
 	}
 
+	/// <summary>
+	/// Creates a new file and writes a sequence of lines.
+	/// </summary>
+	/// <param name="path">The path where the file will be created.</param>
+	/// <param name="lines">The lines to write to the file.</param>
 	public void Create(string path, IEnumerable<string> lines)
 	{
 		if (!File.Exists(path))
@@ -68,6 +98,11 @@ public class FileService : IFileService
 		}
 	}
 
+	/// <summary>
+	/// Overwrites an existing file with specified content under the context of an Active Directory user.
+	/// </summary>
+	/// <param name="path">The path of the file to overwrite.</param>
+	/// <param name="content">The string content to write to the file.</param>
 	public void OverwriteUnderUser(string path, string content)
 	{
 		_activeDirectoryService.ImpersonateUserAndRunAction(() =>
@@ -80,6 +115,11 @@ public class FileService : IFileService
 		});
 	}
 
+	/// <summary>
+	/// Overwrites an existing file with specified content.
+	/// </summary>
+	/// <param name="path">The path of the file to overwrite.</param>
+	/// <param name="content">The string content to write to the file.</param>
 	public void Overwrite(string path, string content)
 	{
 		if (File.Exists(path))
@@ -89,6 +129,11 @@ public class FileService : IFileService
 		File.WriteAllText(path, content);
 	}
 
+	/// <summary>
+	/// Overwrites an existing file with specified byte content under the context of an Active Directory user.
+	/// </summary>
+	/// <param name="path">The path of the file to overwrite.</param>
+	/// <param name="contentBytes">The byte array content to write to the file.</param>
 	public void OverwriteUnderUser(string path, byte[] contentBytes)
 	{
 		_activeDirectoryService.ImpersonateUserAndRunAction(() =>
@@ -101,6 +146,11 @@ public class FileService : IFileService
 		});
 	}
 
+	/// <summary>
+	/// Overwrites an existing file with specified byte content.
+	/// </summary>
+	/// <param name="path">The path of the file to overwrite.</param>
+	/// <param name="contentBytes">The byte array content to write to the file.</param>
 	public void Overwrite(string path, byte[] contentBytes)
 	{
 		if (File.Exists(path))
@@ -110,6 +160,11 @@ public class FileService : IFileService
 		File.WriteAllBytes(path, contentBytes);
 	}
 
+	/// <summary>
+	/// Overwrites an existing file by writing a sequence of lines under the context of an Active Directory user.
+	/// </summary>
+	/// <param name="path">The path of the file to overwrite.</param>
+	/// <param name="lines">The lines to write to the file.</param>
 	public void OverwriteUnderUser(string path, IEnumerable<string> lines)
 	{
 		_activeDirectoryService.ImpersonateUserAndRunAction(() =>
@@ -122,6 +177,11 @@ public class FileService : IFileService
 		});
 	}
 
+	/// <summary>
+	/// Overwrites an existing file by writing a sequence of lines.
+	/// </summary>
+	/// <param name="path">The path of the file to overwrite.</param>
+	/// <param name="lines">The lines to write to the file.</param>
 	public void Overwrite(string path, IEnumerable<string> lines)
 	{
 		if (File.Exists(path))
@@ -131,6 +191,10 @@ public class FileService : IFileService
 		File.WriteAllLines(path, lines);
 	}
 
+	/// <summary>
+	/// Deletes a file at the specified path under the context of an Active Directory user.
+	/// </summary>
+	/// <param name="path">The path of the file to delete.</param>
 	public void DeleteUnderUser(string path)
 	{
 		_activeDirectoryService.ImpersonateUserAndRunAction(() =>
@@ -142,6 +206,10 @@ public class FileService : IFileService
 		});
 	}
 
+	/// <summary>
+	/// Deletes a file at the specified path.
+	/// </summary>
+	/// <param name="path">The path of the file to delete.</param>
 	public void Delete(string path)
 	{
 		if (File.Exists(path))
@@ -150,6 +218,11 @@ public class FileService : IFileService
 		}
 	}
 
+	/// <summary>
+	/// Reads the contents of a file at the specified path as a byte array under the context of an Active Directory user.
+	/// </summary>
+	/// <param name="path">The path of the file to read.</param>
+	/// <returns>A byte array containing the contents of the file.</returns>
 	public byte[] ReadUnderUser(string path)
 	{
 		return _activeDirectoryService.ImpersonateUserAndRunAction(() =>
@@ -163,6 +236,11 @@ public class FileService : IFileService
 		});
 	}
 
+	/// <summary>
+	/// Reads the contents of a file at the specified path as a byte array.
+	/// </summary>
+	/// <param name="path">The path of the file to read.</param>
+	/// <returns>A byte array containing the contents of the file.</returns>
 	public byte[] Read(string path)
 	{
 		if (File.Exists(path))
@@ -173,6 +251,11 @@ public class FileService : IFileService
 		return Array.Empty<byte>();
 	}
 
+	/// <summary>
+	/// Reads the contents of a file at the specified path as a string under the context of an Active Directory user.
+	/// </summary>
+	/// <param name="path">The path of the file to read.</param>
+	/// <returns>A string containing the contents of the file.</returns>
 	public string ReadTextUnderUser(string path)
 	{
 		return _activeDirectoryService.ImpersonateUserAndRunAction(() =>
@@ -186,6 +269,11 @@ public class FileService : IFileService
 		});
 	}
 
+	/// <summary>
+	/// Reads the contents of a file at the specified path as a string.
+	/// </summary>
+	/// <param name="path">The path of the file to read.</param>
+	/// <returns>A string containing the contents of the file.</returns>
 	public string ReadText(string path)
 	{
 		if (File.Exists(path))
@@ -196,6 +284,11 @@ public class FileService : IFileService
 		return string.Empty;
 	}
 
+	/// <summary>
+	/// Reads the lines of a file at the specified path as an enumerable collection of strings under the context of an Active Directory user.
+	/// </summary>
+	/// <param name="path">The path of the file from which to read the lines.</param>
+	/// <returns>An enumerable collection of strings, each representing a line in the file.</returns>
 	public IEnumerable<string> ReadLinesUnderUser(string path)
 	{
 		return _activeDirectoryService.ImpersonateUserAndRunAction(() =>
@@ -209,6 +302,11 @@ public class FileService : IFileService
 		});
 	}
 
+	/// <summary>
+	/// Reads the lines of a file at the specified path as an enumerable collection of strings.
+	/// </summary>
+	/// <param name="path">The path of the file from which to read the lines.</param>
+	/// <returns>An enumerable collection of strings, each representing a line in the file.</returns>
 	public IEnumerable<string> ReadLines(string path)
 	{
 		if (File.Exists(path))
@@ -219,6 +317,11 @@ public class FileService : IFileService
 		return new List<string>();
 	}
 
+	/// <summary>
+	/// Checks if a file exists at the specified path under the context of an Active Directory user.
+	/// </summary>
+	/// <param name="path">The path of the file to check.</param>
+	/// <returns>true if the file exists; otherwise, false.</returns>
 	public bool IsExistsUnderUser(string path)
 	{
 		return _activeDirectoryService.ImpersonateUserAndRunAction(() =>
@@ -227,6 +330,11 @@ public class FileService : IFileService
 		});
 	}
 
+	/// <summary>
+	/// Checks if a file exists at the specified path.
+	/// </summary>
+	/// <param name="path">The path of the file to check.</param>
+	/// <returns>true if the file exists; otherwise, false.</returns>
 	public bool IsExists(string path)
 	{
 		return File.Exists(path);
