1eb2f571ebb699327f49546c6bf649976c093a7d
ShadyNagy <shadynagi@gmail.com>
2024-03-03 14:43:56 +0200

    ActiveDirectoryService xml comments are added.

--- src/ActiveDirectoryFileManagement/Services/ActiveDirectoryService.cs
+++ src/ActiveDirectoryFileManagement/Services/ActiveDirectoryService.cs
@@ -7,6 +7,9 @@ using Microsoft.Win32.SafeHandles;
 
 namespace ActiveDirectoryFileManagement.Services;
 
+/// <summary>
+/// Service for executing actions under the context of an impersonated Active Directory user.
+/// </summary>
 public class ActiveDirectoryService : IActiveDirectoryService
 {
 	private readonly ActiveDirectorySettings _activeDirectorySettings;
@@ -14,19 +17,29 @@ public class ActiveDirectoryService : IActiveDirectoryService
 	private const int LOGON32_LOGON_INTERACTIVE = 2;
 	private const int LOGON32_PROVIDER_DEFAULT = 0;
 
+	/// <summary>
+	/// Initializes a new instance of the <see cref="ActiveDirectoryService"/> class.
+	/// </summary>
+	/// <param name="activeDirectorySettings">Configuration settings for connecting to Active Directory.</param>
 	public ActiveDirectoryService(ActiveDirectorySettings activeDirectorySettings)
 	{
 		_activeDirectorySettings = activeDirectorySettings;
 	}
 
+	/// <summary>
+	/// Executes a function under the context of an impersonated Active Directory user and returns the result.
+	/// </summary>
+	/// <typeparam name="TResult">The type of the result returned by the function.</typeparam>
+	/// <param name="action">The function to execute, which returns a result of type <typeparamref name="TResult"/>.</param>
+	/// <returns>The result of the function executed under the impersonated user context.</returns>
+	/// <exception cref="System.ComponentModel.Win32Exception">Thrown when there is an error logging in the user.</exception>
 	public TResult ImpersonateUserAndRunAction<TResult>(Func<TResult> action)
 	{
 		var userHandle = Login();
 
 		try
 		{
-			TResult result = WindowsIdentity.RunImpersonated(userHandle, action);
-			return result;
+			return WindowsIdentity.RunImpersonated(userHandle, action);
 		}
 		finally
 		{
@@ -34,13 +47,18 @@ public class ActiveDirectoryService : IActiveDirectoryService
 		}
 	}
 
+	/// <summary>
+	/// Executes an action under the context of an impersonated Active Directory user.
+	/// </summary>
+	/// <param name="action">The action to execute under the impersonated user context.</param>
+	/// <exception cref="System.ComponentModel.Win32Exception">Thrown when there is an error logging in the user.</exception>
 	public void ImpersonateUserAndRunAction(Action action)
 	{
 		var userHandle = Login();
 
 		try
 		{
-			WindowsIdentity.RunImpersonated(userHandle, () => { action(); });
+			WindowsIdentity.RunImpersonated(userHandle, () => action());
 		}
 		finally
 		{
@@ -48,9 +66,21 @@ public class ActiveDirectoryService : IActiveDirectoryService
 		}
 	}
 
+	/// <summary>
+	/// Logs in the Active Directory user based on the provided settings and returns the user token handle.
+	/// </summary>
+	/// <returns>A safe token handle for the impersonated user.</returns>
+	/// <exception cref="System.ComponentModel.Win32Exception">Thrown when there is an error during the login process.</exception>
 	private SafeAccessTokenHandle Login()
 	{
-		bool success = Impersonation.LogonUser(_activeDirectorySettings.Username, _activeDirectorySettings.Domain, _activeDirectorySettings.Password, LOGON32_LOGON_INTERACTIVE, LOGON32_PROVIDER_DEFAULT, out SafeAccessTokenHandle userHandle);
+		bool success = Impersonation.LogonUser(
+				_activeDirectorySettings.Username,
+				_activeDirectorySettings.Domain,
+				_activeDirectorySettings.Password,
+				LOGON32_LOGON_INTERACTIVE,
+				LOGON32_PROVIDER_DEFAULT,
+				out SafeAccessTokenHandle userHandle);
+
 		if (!success)
 		{
 			throw new System.ComponentModel.Win32Exception(Marshal.GetLastWin32Error());
